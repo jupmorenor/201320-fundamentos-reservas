520474183c6291334e12c81e98b89b9e86278d52
rimahj7413@hotmail.com <rimahj7413@hotmail.com@fb3623cb-c250-3b61-b189-2fbd3ca5503b>
2013-11-24 02:51:55 +0000

    Ventana Principal y los paneles que la componen

---
+++ trunk/reservas/src/gui/PanelHuesped.java
@@ -0,0 +1,89 @@
+/**
+ * 
+ */
+package gui;
+
+import java.awt.BorderLayout;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.border.TitledBorder;
+
+/**
+ * @author Jhamir
+ *
+ */
+public class PanelHuesped extends JPanel{
+
+	private JLabel lblId;
+	
+	private JLabel lblNombre;
+	
+	private JLabel lblEdad;	
+	
+	private JLabel lblTelefono;	
+	
+	private JLabel lblDireccion;
+	
+	private JTextField txtId;
+	
+	private JTextField txtNombre;
+	
+	private JTextField txtEdad;	
+	
+	private JTextField txtTelefono;
+	
+	private JTextField txtDireccion;
+	
+	/**
+	 * 
+	 */
+	public PanelHuesped() {
+		this.setBorder(new TitledBorder("Información del Huesped"));
+		this.setLayout(new BorderLayout());	
+		this.setLayout(null);
+		
+		this.lblId = new JLabel("Identificación");
+		this.lblId.setBounds(20, 20, 100, 25);
+		
+		this.lblNombre = new JLabel("Nombre");
+		this.lblNombre.setBounds(20, 50, 100, 25);
+		
+		this.lblEdad = new JLabel("Edad");
+		this.lblEdad.setBounds(20, 80, 100, 25);
+		
+		this.lblTelefono = new JLabel("Telefono");
+		this.lblTelefono.setBounds(20, 110, 100, 25);
+		
+		this.lblDireccion = new JLabel("Dirección");
+		this.lblDireccion.setBounds(20, 140, 100, 25);
+		
+		this.txtId = new JTextField();
+		this.txtId.setBounds(130, 20, 100, 25);
+		
+		this.txtNombre = new JTextField();
+		this.txtNombre.setBounds(130, 50, 100, 25);
+		
+		this.txtEdad = new JTextField();
+		this.txtEdad.setBounds(130, 80, 100, 25);
+		
+		this.txtTelefono = new JTextField();
+		this.txtTelefono.setBounds(130, 110, 100, 25);
+		
+		this.txtDireccion = new JTextField();
+		this.txtDireccion.setBounds(130, 140, 100, 25);
+		
+		this.add(lblDireccion);
+		this.add(lblTelefono);
+		this.add(lblNombre);
+		this.add(lblEdad);
+		this.add(lblId);
+		this.add(txtDireccion);
+		this.add(txtId);
+		this.add(txtTelefono);
+		this.add(txtNombre);
+		this.add(txtEdad);
+	}
+
+}
---
+++ trunk/reservas/src/gui/PanelReserva.java
@@ -0,0 +1,66 @@
+/**
+ * 
+ */
+package gui;
+
+import java.awt.BorderLayout;
+
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.border.TitledBorder;
+
+/**
+ * @author Jhamir
+ *
+ */
+public class PanelReserva extends JPanel{
+	
+	private JLabel lblNumero;
+	
+	private JLabel lblDiasReservados;
+	
+	private JLabel lblFechaInicio;
+	
+	private JTextField txtNumero;
+	
+	private JTextField txtDiasReservados;
+	
+	private JTextField txtFechaInicio;
+
+	/**
+	 * 
+	 */
+	public PanelReserva() {
+		this.setBorder(new TitledBorder("Información de la Reserva"));
+		this.setLayout(new BorderLayout());	
+		this.setLayout(null);
+		
+		this.lblNumero = new JLabel("Número");
+		this.lblNumero.setBounds(20, 20, 100, 25);
+		
+		this.lblDiasReservados = new JLabel("Dias Reservados");
+		this.lblDiasReservados.setBounds(20, 50, 100, 25);
+		
+		this.lblFechaInicio = new JLabel("Fecha Inicio");
+		this.lblFechaInicio.setBounds(20, 80, 100, 25);
+		
+		this.txtNumero = new JTextField();
+		this.txtNumero.setBounds(130, 20, 100, 25);
+		
+		this.txtDiasReservados = new JTextField();
+		this.txtDiasReservados.setBounds(130, 50, 100, 25);
+		
+		this.txtFechaInicio = new JTextField();
+		this.txtFechaInicio.setBounds(130, 80, 100, 25);
+		
+		this.add(lblDiasReservados);
+		this.add(lblFechaInicio);
+		this.add(lblNumero);
+		this.add(txtNumero);
+		this.add(txtDiasReservados);
+		this.add(txtFechaInicio);
+	}
+
+}
